fdc7cf126f5ac9b2451a685a77a2ce202ff9633d
julianaxalves <alvesjuliana944@gmail.com>
2025-04-11 17:42:45 -0300

    script.js

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ function verificarSenha() {
     let d3 = Number(document.getElementById("d3").value);
     let d4 = Number(document.getElementById("d4").value);
 
-    if (d1 === "7") {
+    if (d1 === 7) {
         document.getElementById("d1").style.color = "green";
     } else {
         document.getElementById("d1").style.color = "red";
     }
 
-    if (d2 === "9") {
+    if (d2 === 9) {
         document.getElementById("d2").style.color = "green";
     } else {
         document.getElementById("d2").style.color = "red";
     }
 
-    if (d3 === "8") {
+    if (d3 === 8) {
         document.getElementById("d3").style.color = "green";
     } else {
         document.getElementById("d3").style.color = "red";
     }
 
-    if (d4 === "2") {
+    if (d4 === 2) {
         document.getElementById("d4").style.color = "green";
     } else {
         document.getElementById("d4").style.color = "red";
     }
 
-    if (d1 === "7" && d2 === "9" && d3 === "8" && d4 === "2") {
+    if (d1 === 7 && d2 === 9 && d3 === 8 && d4 === 2) {
         mensagem.textContent = "Senha correta. Acesso permitido."
         mensagem.style.color = "black";
         d1.disabled = true;
