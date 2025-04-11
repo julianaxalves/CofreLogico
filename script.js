ca3f0065650168fc1a26c31dff1a671906abfd83
julianaxalves <alvesjuliana944@gmail.com>
2025-04-11 15:08:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function verificarSenha() {
+    let d1 = Number(document.getElementById("d1").value);
+    let d2 = Number(document.getElementById("d2").value);
+    let d3 = Number(document.getElementById("d3").value);
+    let d4 = Number(document.getElementById("d4").value);
+
+    if (d1 === "7") {
+        document.getElementById("d1").style.color = "green";
+    } else {
+        document.getElementById("d1").style.color = "red";
+    }
+
+    if (d2 === "9") {
+        document.getElementById("d2").style.color = "green";
+    } else {
+        document.getElementById("d2").style.color = "red";
+    }
+
+    if (d2 === "8") {
+        document.getElementById("d3").style.color = "green";
+    } else {
+        document.getElementById("d3").style.color = "red";
+    }
+
+    if (d2 === "2") {
+        document.getElementById("d4").style.color = "green";
+    } else {
+        document.getElementById("d4").style.color = "red";
+    }
+
+    if (d1 === "7" && d2 === "9" && d3 === "8" && d4 === "2") {
+        mensagem.textContent = "Senha correta. Acesso permitido."
+        mensagem.style.color = "black";
+        d1.disabled = true;
+        d2.disabled = true;
+        d3.disabled = true;
+        d4.disabled = true;
+    } else {
+        document.body.style.backgroundColor = "red";
+        alert("Acesso negado!")
+    }
+}
