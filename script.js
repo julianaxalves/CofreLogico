6e93d061f79ef59d6414d82bc62b6ab3e1683fdc
julianaxalves <alvesjuliana944@gmail.com>
2025-04-24 14:45:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function irparaCofre() {
-    document.getElementBtId("fimdeJogo").style.display = "none";
+    document.getElementById("fimdeJogo").style.display = "none";
     document.querySelector(".telaInicial").style.display = "none";
     document.querySelector("#conteudo").style.display = "flex";
 }
